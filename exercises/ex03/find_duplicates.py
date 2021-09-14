ca3aff728b792bf6b986a99eb1b83b3a07dc0414
Gabby Lopez <gml0426@email.unc.edu>
2021-09-14 13:40:34 -0400

    final draft

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,17 @@
 """Finding duplicate letters in a word."""
 
 __author__ = "123456789"
+
+word: str = str(input("Enter a word: "))
+i: int = 0
+j: int = 0
+output: bool = False
+while i < len(word):
+    j = i + 1
+    while j < len(word):
+        if word[i] == word[j]:
+            output = True
+        j += 1
+    i += 1
+        
+print ("Found duplicate: " + str(output))
